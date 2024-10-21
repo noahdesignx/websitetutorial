43ce21427c6fa5e77bd24d71725c401b98682f68
Noah Mitchell <161106211+noahdesignx@users.noreply.github.com>
2024-10-20 17:33:14 -0700

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,11 @@
+//Animate on Scroll Functionality
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
